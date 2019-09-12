184cc13e1c528ae41c39ccdba87b385a327e0343
Aarni Koskela <akx@iki.fi>
2019-09-12 14:43:27 +0300

    DRY out inputs

--- valohai_utils/inputs.py
+++ valohai_utils/inputs.py
@@ -5,10 +5,7 @@ import valohai_utils.paths as config
 
 
 def get_input_path(name, default=""):
-    files = get_input_paths(name)
-    if len(files) > 0:
-        return files[0]
-    return default
+    return get_input_paths(name, [default])[0]
 
 
 def get_input_paths(name, default=None):
@@ -18,10 +15,9 @@ def get_input_paths(name, default=None):
     if os.path.isfile(inputs_config_path):
         with open(inputs_config_path) as json_file:
             data = json.load(json_file)
-            if name in data:
-                if len(data[name]["files"]) > 0:
-                    result = []
-                    for file in data[name]["files"]:
-                        result.append(file["path"])
-                    return result
+        input_info = data.get(name)
+        if input_info:
+            files = input_info.get('files', ())
+            if files:
+                return [file['path'] for file in files]
     return default
