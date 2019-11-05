fd46d0238d928ee52e293732d7d240623ee314ce
Aarni Koskela <akx@iki.fi>
2019-11-05 18:25:42 +0200

    WIP

--- valohai_utils/inputs.py
+++ valohai_utils/inputs.py
@@ -1,8 +1,9 @@
 import json
 import os
-from typing import List, Optional
+from typing import IO, Iterable, List, Optional
 
 from . import paths
+from .internals import vfs
 from .internals.input_info import InputInfo
 
 
@@ -28,3 +29,32 @@ def get_input_paths(name, default=None) -> List[str]:
 
 def get_input_path(name, default="") -> str:
     return get_input_paths(name, [default])[0]
+
+
+def get_input_vfs(name, process_archives: bool = True) -> vfs.VFS:
+    v = vfs.VFS()
+    ii = get_input_info(name)
+    for file_info in ii.files:
+        vfs.add_disk_file(
+            v,
+            name=file_info.name,
+            path=file_info.path,
+            process_archives=process_archives,
+        )
+    return v
+
+
+def get_input_file_paths(name) -> Iterable[str]:
+    # TODO: this needs a filter parameter
+    # TODO: This name is shitty, c.f. get_input_paths()
+    # TODO: How to clean up the VFS's concrete files?
+    for file in get_input_vfs(name):
+        if isinstance(file, vfs.FileInContainer):
+            yield file.open_concrete(delete=False).name
+        elif isinstance(file, vfs.FileOnDisk):
+            yield file.path
+
+
+def get_input_file_streams(name) -> Iterable[IO]:
+    for file in get_input_vfs(name):
+        yield file.open()
