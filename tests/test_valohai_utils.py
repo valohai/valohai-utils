31221ae8282341ef6e5f2c3d546aaadb8480adb5
Aarni Koskela <akx@iki.fi>
2019-11-05 18:24:32 +0200

    Embetter inputs tests

--- tests/test_valohai_utils.py
+++ tests/test_valohai_utils.py
@@ -1,14 +1,4 @@
-from valohai_utils.inputs import get_input_path
 from valohai_utils.parameters import get_parameter
-
-
-def test_get_input_paths(use_test_config_dir):
-    assert get_input_path("herp") == "/valohai/inputs/herp/Example.jpg"
-    assert (
-        get_input_path("herp", "unused_default") == "/valohai/inputs/herp/Example.jpg"
-    )
-    assert get_input_path("nonono") == ""
-    assert get_input_path("nonono", "default_path_123") == "default_path_123"
 
 
 def test_get_parameters(use_test_config_dir):
