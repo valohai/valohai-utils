6b3df1fd4473283bf1027ba47ef6fb21baae1618
Juha Kiili <juha@juhakiili.com>
2019-12-03 16:17:06 +0200

    Tests for AST parsing

---
+++ tests/test_parsing/test1.py
@@ -0,0 +1,23 @@
+import os
+import valohai
+
+params = {
+  "param1": True,
+  "param2": "asdf",
+  "param3": 123,
+  "param4": 0.0001,
+}
+
+inputs = {
+  "input1": "asdf",
+  "input2": ["yolol", "yalala"]
+}
+
+def prepare(a, b):
+  print("this is fake method %s %s" % (a, b))
+
+
+prepare("this should not be parsed", "ever")
+valohai.utils.prepare(step="this should not be parsed either")
+valohai.prepare(step="foobar1", parameters=params, inputs=inputs)
+
