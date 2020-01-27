e1bcf5a5ed4424b893b4ca1b7f46f33a3f9c058a
Juha Kiili <juha@juhakiili.com>
2020-01-27 16:45:59 +0200

    Set paths of config, inputs, outputs for local runs

--- valohai/paths.py
+++ valohai/paths.py
@@ -1,23 +1,25 @@
 import os
 
+from valohai.config import is_running_in_valohai
+
 
 def get_config_path():
-    return os.environ.get("VH_CONFIG_DIR", "/valohai/config")
+    return os.environ.get("VH_CONFIG_DIR", "/valohai/config" if is_running_in_valohai() else ".valohai/config")
 
 
 def get_inputs_path(input_name=None):
-    path = os.environ.get("VH_INPUTS_DIR", "/valohai/inputs")
+    path = os.environ.get("VH_INPUTS_DIR", "/valohai/inputs" if is_running_in_valohai() else ".valohai/inputs")
     if input_name:
         path = os.path.join(path, input_name)
     return path
 
 
 def get_outputs_path():
-    return os.environ.get("VH_INPUTS_DIR", "/valohai/outputs")
+    return os.environ.get("VH_INPUTS_DIR", "/valohai/outputs" if is_running_in_valohai() else ".valohai/outputs")
 
 
 def get_repository_path():
-    return os.environ.get("VH_REPOSITORY_DIR", "/valohai/repository")
+    return os.environ.get("VH_REPOSITORY_DIR", "/valohai/outputs" if is_running_in_valohai() else ".")
 
 
 def get_inputs_config_path():
