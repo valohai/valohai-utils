630e432011cfccd29ddb80a5ea6d8bfde385a60f
Juha Kiili <juha@juhakiili.com>
2020-01-28 16:04:07 +0200

    Move local paths as module wide consts and have unique output path per execution

---
+++ valohai/consts.py
@@ -0,0 +1,4 @@
+VH_LOCAL_CONFIG_DIR = ".valohai/config"
+VH_LOCAL_INPUTS_DIR = ".valohai/inputs"
+VH_LOCAL_OUTPUTS_DIR = ".valohai/outputs"
+VH_LOCAL_REPOSITORY_DIR = "."
--- valohai/paths.py
+++ valohai/paths.py
@@ -1,25 +1,32 @@
 import os
+import uuid
 
 from valohai.config import is_running_in_valohai
+from valohai.consts import VH_LOCAL_CONFIG_DIR, VH_LOCAL_INPUTS_DIR, VH_LOCAL_OUTPUTS_DIR
+
+execution_guid = None
 
 
 def get_config_path():
-    return os.environ.get("VH_CONFIG_DIR", "/valohai/config" if is_running_in_valohai() else ".valohai/config")
+    return os.environ.get("VH_CONFIG_DIR", "/valohai/config" if is_running_in_valohai() else VH_LOCAL_CONFIG_DIR)
 
 
 def get_inputs_path(input_name=None):
-    path = os.environ.get("VH_INPUTS_DIR", "/valohai/inputs" if is_running_in_valohai() else ".valohai/inputs")
+    path = os.environ.get("VH_INPUTS_DIR", "/valohai/inputs" if is_running_in_valohai() else VH_LOCAL_INPUTS_DIR)
     if input_name:
         path = os.path.join(path, input_name)
     return path
 
 
 def get_outputs_path():
-    return os.environ.get("VH_INPUTS_DIR", "/valohai/outputs" if is_running_in_valohai() else ".valohai/outputs")
+    if is_running_in_valohai():
+        return os.environ.get("VH_OUTPUTS_DIR", "/valohai/outputs")
+    else:
+        return os.path.join(VH_LOCAL_OUTPUTS_DIR, get_execution_guid())
 
 
 def get_repository_path():
-    return os.environ.get("VH_REPOSITORY_DIR", "/valohai/outputs" if is_running_in_valohai() else ".")
+    return os.environ.get("VH_REPOSITORY_DIR", "/valohai/repository" if is_running_in_valohai() else ".")
 
 
 def get_inputs_config_path():
@@ -28,3 +35,10 @@ def get_inputs_config_path():
 
 def get_parameters_config_path():
     return os.path.join(get_config_path(), "parameters.json")
+
+
+def get_execution_guid():
+    global execution_guid
+    if not execution_guid:
+        execution_guid = uuid.uuid4().hex
+    return execution_guid
