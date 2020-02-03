e4432b983b3fbd38490409336c9f008c765f4f00
Juha Kiili <juha@juhakiili.com>
2020-02-03 14:20:51 +0200

    Refactor parsing tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,11 +1,44 @@
+import glob
+import json
+import os
+from collections import namedtuple
+
+import pytest
+
 from valohai.internals.parsing import parse
 
 
-def test_parameters_parse(source_codes):
-    for test_item in source_codes:
-        print(test_item['source'])
-        result = parse(test_item['source'])
+def read_test_data():
+    """
+    Expected files (tests/test_parsing):
+        mytest.py -- Python file calling valohai.prepare()
+        mytest.inputs.json -- Expected parsed inputs
+        mytest.parameters.json -- Expected parsed parameters
+        mytest.step.json -- Expected parsed step
 
-        assert result.parameters == test_item['parameters']
-        assert result.inputs == test_item['inputs']
-        assert result.step == test_item["step"]['name']
+    """
+    test_data = []
+    for source_path in glob.glob("tests/test_parsing/*.py"):
+        dirname = os.path.dirname(source_path)
+        name, extension = os.path.splitext(os.path.basename(source_path))
+        prefix = os.path.join(dirname, name)
+        with open("%s.py" % prefix, "r") as source_python, \
+            open("%s.parameters.json" % prefix, "r") as parameters_json, \
+            open("%s.inputs.json" % prefix, "r") as inputs_json, \
+            open("%s.step.json" % prefix, "r") as step_json:
+            test_data.append((
+                source_python.read(),
+                json.loads(inputs_json.read()),
+                json.loads(parameters_json.read()),
+                json.loads(step_json.read())['name']
+            ))
+    return test_data
+
+
+@pytest.mark.parametrize("source, inputs, parameters, step", read_test_data())
+def test_parse(source, inputs, parameters, step):
+    result = parse(source)
+
+    assert result.inputs == inputs
+    assert result.parameters == parameters
+    assert result.step == step
