b5064bc0f0a0a89fb4518bdcfede5283d7da74bf
Juha Kiili <juha@juhakiili.com>
2020-02-04 15:09:39 +0200

    Simple logging API + tests

---
+++ tests/test_logs.py
@@ -0,0 +1,32 @@
+from valohai.logs import log, log_partial, flush_logs
+
+
+def test_basic_logging(capsys):
+    log("myint", 123)
+    captured = capsys.readouterr()
+    assert captured.out == "{\"myint\": 123}\n"
+
+    log("I am float", 0.1241234435877)
+    captured = capsys.readouterr()
+    assert captured.out == "{\"I am float\": 0.1241234435877}\n"
+
+
+def test_logging_with_unsupported_types(capsys):
+    log("herp", ["asdif", 234, None])
+    captured, err = capsys.readouterr()
+    assert err == "Warning: Value of the logged item (herp) is not of the expected type (int, str, float)."
+
+
+def test_partial_logging(capsys):
+    log_partial("myint", 123)
+    log_partial("myfloat", 0.5435)
+    flush_logs()
+    captured = capsys.readouterr()
+    assert captured.out == "{\"myint\": 123, \"myfloat\": 0.5435}\n"
+
+    log_partial("myint", 234)
+    log_partial("myfloat", 0.2322323)
+    log_partial("extrathing", 2)
+    flush_logs()
+    captured = capsys.readouterr()
+    assert captured.out == "{\"myint\": 234, \"myfloat\": 0.2322323, \"extrathing\": 2}\n"
