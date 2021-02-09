4332419a05e4f42a832ae40c702d84de33dabcc0
Juha Kiili <juha@valohai.com>
2021-02-09 09:53:28 +0200

    Rename logs to metadata

--- valohai/__init__.py
+++ valohai/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.1.1"
 from .utils import prepare
 from .inputs import inputs
 from .outputs import outputs
-from .logs import logger
+from .metadata import logger
 from .parameters import parameters
