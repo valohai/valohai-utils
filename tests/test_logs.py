76df331302bed4dfe0b8f394c7bf0146b34902f6
Juha Kiili <juha@juhakiili.com>
2021-02-10 16:36:09 +0200

    Fixes for logging

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -13,13 +13,6 @@ def test_basic_logging(capsys):
     assert captured.out == "{\"I am float\": 0.1241234435877}\n"
 
 
-def test_logging_with_unsupported_types(capsys):
-    with valohai.logger() as logger:
-        logger.log("herp", ["asdif", 234, None])
-    captured, err = capsys.readouterr()
-    assert "Warning: Value of the logged item (herp) is not of the expected type (int, str, float)." in err
-
-
 def test_partial_logging(capsys):
     with valohai.logger() as logger:
         logger.log("myint", 123)
@@ -31,6 +24,6 @@ def test_partial_logging(capsys):
     logger.log("myint", 234)
     logger.log("myfloat", 0.2322323)
     logger.log("extrathing", 2)
-    logger.flush_logs()
+    logger.flush()
     captured = capsys.readouterr()
     assert captured.out == "{\"myint\": 234, \"myfloat\": 0.2322323, \"extrathing\": 2}\n"
--- valohai/metadata.py
+++ valohai/metadata.py
@@ -1,10 +1,9 @@
 import json
-import sys
 
 _supported_types = [int, float]
 
 
-class logger(object):
+class Logger(object):
     def __init__(self):
         self.partial_logs = {}
 
@@ -13,9 +12,9 @@ class logger(object):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.flush_logs()
+        self.flush()
 
-    def log(self, name, value):
+    def log(self, *args, **kwargs):
         """Log a single name/value pair to be flushed into standard output later as batch.
 
         For a repeating iteration like a machine learning training loop, Valohai expects
@@ -36,17 +35,26 @@ class logger(object):
                 logger.log("loss", 0.123)
                 logger.flush_logs()
 
-        Both examples will log all three metrics at once at the end of each epoch.
+        Example 3:
+            for epoch in range(10):
+                with logger as valohai.logger():
+                    logger.log("epoch", epoch)
+                    logger.log(acc=0.54, loss=0.123)
+
+        All three examples will act exactly the same.
 
         :param name: Name of the variable being logged (example: learning_rate)
         :param value: Value of the logged variable
 
         """
-        serializable = self._get_serializable(name, value)
-        if serializable:
-            self.partial_logs.update(serializable)
+        if len(args) % 2 != 0:
+            raise ValueError(f"Odd number of arguments in {args} for log()")
+        for key, value in zip(args[::2], args[1::2]):
+            self._serialize(key, value)
+        for key, value in kwargs.items():
+            self._serialize(key, value)
 
-    def flush_logs(self):
+    def flush(self):
         """Flush all the partial logs into standard as a batch.
 
         For a repeating iteration like a machine learning training loop, Valohai expects
@@ -62,13 +70,11 @@ class logger(object):
         This will log all three metrics at once.
         """
         if self.partial_logs:
-            print(json.dumps(self.partial_logs))
+            print(json.dumps(self.partial_logs, default=str))
             self.partial_logs.clear()
 
-    def _get_serializable(self, name, value):
-        if isinstance(value, (int, str, float)):
-            return {str(name): value}
+    def _serialize(self, name, value):
+        self.partial_logs.update({str(name): value})
 
-        print(f"Warning: Value of the logged item ({name}) is not of the expected type (int, str, float).",
-              file=sys.stderr)
-        return None
+
+logger = Logger
