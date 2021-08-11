9f22ce68a9fad9da58df274b364f4d06ddf3da96
Juha Kiili <juha@valohai.com>
2021-08-11 18:18:13 +0300

    Change wikimedia.org urls to our own s3 urls for the tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -12,16 +12,18 @@ def test_download(tmpdir, monkeypatch, requests_mock):
     requests_mock.get(
         "https://valohai-mnist.s3.amazonaws.com/t10k-images-idx3-ubyte.gz"
     )
-    requests_mock.get("https://upload.wikimedia.org/wikipedia/commons/8/84/Example.svg")
     requests_mock.get(
-        "https://upload.wikimedia.org/wikipedia/commons/0/01/Example_Wikipedia_sandbox_move_UI.png"
+        "https://valohai-mnist.s3.amazonaws.com/train-images-idx3-ubyte.gz"
+    )
+    requests_mock.get(
+        "https://valohai-mnist.s3.amazonaws.com/train-labels-idx1-ubyte.gz"
     )
 
     inputs = {
         "example": "https://valohai-mnist.s3.amazonaws.com/t10k-images-idx3-ubyte.gz",
-        "myimages": [
-            "https://upload.wikimedia.org/wikipedia/commons/8/84/Example.svg",
-            "https://upload.wikimedia.org/wikipedia/commons/0/01/Example_Wikipedia_sandbox_move_UI.png",
+        "mnist": [
+            "https://valohai-mnist.s3.amazonaws.com/train-images-idx3-ubyte.gz",
+            "https://valohai-mnist.s3.amazonaws.com/train-labels-idx1-ubyte.gz",
         ],
     }
 
@@ -34,12 +36,12 @@ def test_download(tmpdir, monkeypatch, requests_mock):
         == "https://valohai-mnist.s3.amazonaws.com/t10k-images-idx3-ubyte.gz"
     )
     assert (
-        load_input_info("myimages").files[0].uri
-        == "https://upload.wikimedia.org/wikipedia/commons/8/84/Example.svg"
+        load_input_info("mnist").files[0].uri
+        == "https://valohai-mnist.s3.amazonaws.com/train-images-idx3-ubyte.gz"
     )
     assert (
-        load_input_info("myimages").files[1].uri
-        == "https://upload.wikimedia.org/wikipedia/commons/0/01/Example_Wikipedia_sandbox_move_UI.png"
+        load_input_info("mnist").files[1].uri
+        == "https://valohai-mnist.s3.amazonaws.com/train-labels-idx1-ubyte.gz"
     )
 
     assert requests_mock.call_count == 3
@@ -47,11 +49,13 @@ def test_download(tmpdir, monkeypatch, requests_mock):
     assert os.path.isfile(
         os.path.join(inputs_dir, "example", "t10k-images-idx3-ubyte.gz")
     )
-    assert os.path.isfile(os.path.join(inputs_dir, "myimages", "Example.svg"))
     assert os.path.isfile(
-        os.path.join(inputs_dir, "myimages", "Example_Wikipedia_sandbox_move_UI.png")
+        os.path.join(inputs_dir, "mnist", "train-images-idx3-ubyte.gz")
+    )
+    assert os.path.isfile(
+        os.path.join(inputs_dir, "mnist", "train-labels-idx1-ubyte.gz")
     )
 
     # Second time around, the file should be cached and not trigger any more downloads
-    load_input_info("myimages")
+    load_input_info("mnist")
     assert requests_mock.call_count == 3
