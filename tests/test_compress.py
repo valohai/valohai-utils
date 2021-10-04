85636f37cf11198008a222243deb393460de80b7
Juha Kiili <juha@valohai.com>
2021-10-04 14:58:24 +0300

    Recursively compress output with ** wildcards and preserve the folder structure (fixes #58)

--- tests/test_compress.py
+++ tests/test_compress.py
@@ -5,11 +5,12 @@ import zipfile
 import pytest
 
 import valohai
+from tests.conftest import create_files
 
 
 @pytest.mark.parametrize("format", ("zip", "tar", "tar.gz"))
 @pytest.mark.parametrize("remove_originals", (False, True))
-def test_compress(outputs_path, output_files, format, remove_originals):
+def test_compress(output_files, format, remove_originals):
     filename = f"hello.{format}"
     package_path = valohai.outputs("morjes").compress(
         output_files, filename, remove_originals=remove_originals
@@ -25,3 +26,34 @@ def test_compress(outputs_path, output_files, format, remove_originals):
     elif "tar" in format:
         with tarfile.open(package_path, "r:*") as tf:
             assert len(list(tf))
+
+
+@pytest.mark.parametrize("format", ("zip", "tar", "tar.gz"))
+@pytest.mark.parametrize("remove_originals", (False, True))
+@pytest.mark.parametrize("filter, expected_files", [("**", 6), ("**/*.jpg", 2)])
+def test_compress_wildcards(tmpdir, format, remove_originals, filter, expected_files):
+    source_dir = tmpdir.strpath
+    created_files = create_files(source_dir)
+
+    filename = f"hello.{format}"
+    package_path = valohai.outputs("foo").compress(
+        source=os.path.join(source_dir, filter),
+        filename=filename,
+        remove_originals=remove_originals,
+    )
+
+    for path in created_files:
+        # picture.jpg should be always compressed with our filter(s)
+        if "picture.jpg" in path:
+            assert os.path.isfile(path) != remove_originals
+
+    if format == "zip":
+        with zipfile.ZipFile(package_path) as zf:
+            assert len(zf.namelist()) == expected_files
+            # picture.jpg should be always compressed with our filter(s)
+            assert "folder/picture.jpg" in zf.namelist()
+    elif "tar" in format:
+        with tarfile.open(package_path, "r:*") as tf:
+            assert len(list(tf)) == expected_files
+            # picture.jpg should be always compressed with our filter(s)
+            assert "folder/picture.jpg" in [tarinfo.name for tarinfo in list(tf)]
--- valohai/internals/files.py
+++ valohai/internals/files.py
@@ -2,7 +2,7 @@ import glob
 import os
 import pathlib
 from stat import S_IREAD, S_IRGRP, S_IROTH
-from typing import Set, Union
+from typing import Set, Tuple, Union
 
 
 def set_file_read_only(path: str) -> None:
@@ -16,16 +16,36 @@ def get_glob_pattern(source: str) -> str:
     return source
 
 
-def expand_globs(sources: Union[str, list], preprocessor=lambda s: s) -> Set[str]:
+def expand_globs(
+    sources: Union[str, list], preprocessor=lambda s: s
+) -> Set[Tuple[str, str]]:
+    """Returns a set of paths as a result of expanding all the source wildcards
+
+    First item of the resulting tuple item is the expanded path
+    Second item of the resulting tuple is the logical root path
+
+    Example
+    source: /tmp/foo/**/*.png
+    output: set(("/tmp/foo/bar/hello.png", "/tmp/foo"), ("/tmp/foo/bar/sub/jello.png", "/tmp/foo"))
+
+    source: /tmp/yeah/*.png
+    output: set(("/tmp/yeah/hello.png", "/tmp/yeah"), ("/tmp/yeah/jello.png", "/tmp/yeah"))
+
+    :param sources: Path or list of paths. May contain * or ** wildcards.
+    :param preprocessor: Preprocessor to be used for each of the paths
+    :return:
+    """
+
     if isinstance(sources, str):
         sources = [sources]
     files_to_compress = set()
     for source_path in sources:
         source_path = preprocessor(source_path)
         glob_pattern = get_glob_pattern(source_path)
-        for file_path in glob.glob(glob_pattern):
+        root_path = os.path.dirname(glob_pattern).split("*", 1)[0]  # Handles ** also
+        for file_path in glob.glob(glob_pattern, recursive=True):
             if os.path.isfile(file_path):
-                files_to_compress.add(file_path)
+                files_to_compress.add((file_path, root_path))
     if not files_to_compress:
         raise ValueError(f"No files to compress at {sources}")
     return files_to_compress
