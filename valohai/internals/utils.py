3bdd79d33a0882e8de254f290cab3ff1e281c7f7
magdapoppins <stenius.mimmy@gmail.com>
2021-12-22 16:02:18 +0200

    Don't treat booleans as flags
    Fixes #78

--- valohai/internals/utils.py
+++ valohai/internals/utils.py
@@ -1,2 +1,8 @@
 def uri_to_filename(uri: str) -> str:
     return uri.rpartition("/")[-1].split("?", 1)[0]
+
+
+def string_to_bool(value: str) -> bool:
+    if value.lower() == "false":
+        return False
+    return True
