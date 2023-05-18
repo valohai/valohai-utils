b8b9ef23967f1eba3a0b04cd23379661815ae864
memona <memona@valohai.com>
2023-05-19 00:55:38 +0300

    Add check sha256 hash function

--- valohai/internals/utils.py
+++ valohai/internals/utils.py
@@ -6,3 +6,14 @@ def string_to_bool(value: str) -> bool:
     if value.lower() == "false":
         return False
     return True
+
+
+def check_sha256_hashing(filepath: str) -> ...:
+    try:
+        from valohai_cli.utils.hashing import get_fp_sha256
+    except ImportError as ie:
+        raise RuntimeError(f"The `valohai-cli` module must be available "
+                f"for verifying hash") from ie
+
+    with open(filepath, 'rb') as f:
+        return get_fp_sha256(f)
