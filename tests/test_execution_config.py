6a4ffba4e06886490add3f389ce894d90e1c60d1
Tero Ykspetäjä <tero@valohai.com>
2025-01-03 13:07:05 +0200

    Add execution configuration helper
    Allow fetching basic execution information (such as the ID of the
    currently running execution) more easily (and without having to know
    where and how the configuration is stored).
    
    Only return some very basic information for now -- more info, and other
    helpers besides fetching the configuration may be added later, if/when
    they are needed.

---
+++ tests/test_execution_config.py
@@ -0,0 +1,61 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from valohai.execution import get_config
+from valohai.paths import get_config_path
+
+# execution configuration example
+EXAMPLE_CONFIG = {
+    "valohai.commit-identifier": "~8603a0246c0397770945439249084fa514eab09548a88dfcb16a019dfe420301",
+    "valohai.creator-email": None,
+    "valohai.creator-id": 1,
+    "valohai.creator-name": "test-user",
+    "valohai.environment-id": "0189ba6d-b1f5-4b64-1a08-9ff09f447034",
+    "valohai.environment-name": "The Default Environment",
+    "valohai.environment-slug": "default",
+    "valohai.execution-counter": 42,
+    "valohai.execution-ctime": "2024-12-30T13:58:46.832605+00:00",
+    "valohai.execution-duration": None,
+    "valohai.execution-id": "019417dc-b12f-0021-c074-87370badadef",
+    "valohai.execution-image": "ghcr.io/astral-sh/uv:python3.12-bookworm-slim",
+    "valohai.execution-qtime": None,
+    "valohai.execution-status": "created",
+    "valohai.execution-step": "exec-step-name",
+    "valohai.execution-tags": [],
+    "valohai.execution-title": "example execution title",
+    "valohai.project-id": "01931b1d-9db0-0021-c074-87370badadef",
+    "valohai.project-name": "test-org/test-project",
+}
+
+
+def test_config_does_not_exist():
+    config_path = Path(get_config_path())
+    assert (
+        not config_path.exists()
+    ), "Config file should not exist by default when not running in Valohai"
+
+    # try reading a non-existent config file
+    assert get_config() is None, "Execution should not exist"
+
+
+def test_get_config(fake_config):
+    fake_config.write_text(json.dumps(EXAMPLE_CONFIG))
+
+    config = get_config()
+    assert config is not None, "Execution config should exist"
+
+    assert config.id == EXAMPLE_CONFIG["valohai.execution-id"]
+    assert config.title == EXAMPLE_CONFIG["valohai.execution-title"]
+    assert config.counter == EXAMPLE_CONFIG["valohai.execution-counter"]
+
+
+@pytest.fixture
+def fake_config(tmp_path, monkeypatch) -> Path:
+    """Create and use a fake execution configuration file."""
+    config_file = tmp_path / "execution.json"
+    # patch the config path to point to the fake file
+    monkeypatch.setenv("VH_CONFIG_DIR", str(tmp_path))
+
+    return config_file
