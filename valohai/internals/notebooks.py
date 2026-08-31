4c08369cb4c61168681245acec54bc9245a97c0c
Aarni Koskela <akx@iki.fi>
2026-08-31 10:37:40 +0300

    Notebook interop: Read JSON, not YAML

--- valohai/internals/notebooks.py
+++ valohai/internals/notebooks.py
@@ -56,7 +56,7 @@ def get_notebook_command(notebook_relative_path: str) -> list[str]:
     _notebook_dir, notebook_name = os.path.split(notebook_relative_path)
     papermill_command = " ".join(
         [
-            "papermill -k python3 -f /valohai/config/parameters.yaml",
+            "papermill -k python3 -f /valohai/config/parameters.json",
             shlex.quote(
                 f"/valohai/repository/{notebook_relative_path.replace(os.sep, '/')}"
             ),
